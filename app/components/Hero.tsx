272c5a2d32ce965089a099fa71c520534018fe8c
he4a00 <https://github.com/he4a00>
2023-12-31 01:51:21 +0200

    edited hero button

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,10 +1,14 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
-import { UserRoundCheck } from "lucide-react";
+import { Book, UserRoundCheck } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import hero from "../images/hero.png";
+import { useUserContext } from "../context/UserContext";
 
 const Hero = () => {
+  const { user }: any = useUserContext();
   return (
     <div className="flex flex-col md:flex-row justify-between items-center p-28 container">
       <div>
@@ -27,15 +31,27 @@ const Hero = () => {
           منصة مختصة في الغوص في
           <br /> عالم الرياضيات
         </p>
-        <Link href="/sign-in">
-          <Button
-            variant="destructive"
-            className="flex gap-2 p-7 transition-all duration-300 ease-in-out transform hover:shadow-md hover:translate-y-0.5"
-          >
-            تسجيل الدخول
-            <UserRoundCheck />
-          </Button>
-        </Link>
+        {!user ? (
+          <Link href="/sign-in">
+            <Button
+              variant="destructive"
+              className="flex gap-2 p-7 transition-all duration-300 ease-in-out transform hover:shadow-md hover:translate-y-0.5"
+            >
+              تسجيل الدخول
+              <UserRoundCheck />
+            </Button>
+          </Link>
+        ) : (
+          <Link href={`/lessons/user/${user?.user?._id}`}>
+            <Button
+              variant="destructive"
+              className="flex gap-2 p-7 transition-all duration-300 ease-in-out transform hover:shadow-md hover:translate-y-0.5"
+            >
+              دروسي
+              <Book />
+            </Button>
+          </Link>
+        )}
       </div>
     </div>
   );

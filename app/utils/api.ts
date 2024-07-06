5508cc0e83749b1a4e409d94773d3cdbe7bfb968
he4a00 <https://github.com/he4a00>
2024-07-06 18:22:37 +0300

    fixed production api

--- app/utils/api.ts
+++ app/utils/api.ts
@@ -14,8 +14,6 @@ const API_BASE_URL =
     ? process.env.NEXT_PUBLIC_API_BASE_URL_LOCAL
     : process.env.NEXT_PUBLIC_API_BASE_URL_REMOTE;
 
-console.log("API_BASE_URL:", API_BASE_URL);
-
 const api = axios.create({
   baseURL: API_BASE_URL,
   withCredentials: true,

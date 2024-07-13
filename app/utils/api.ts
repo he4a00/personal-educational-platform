4fd91d19b5dbb139f9364b030ea3dd35fcfad377
he4a00 <https://github.com/he4a00>
2024-07-13 23:40:25 +0300

    it will owrk

--- app/utils/api.ts
+++ app/utils/api.ts
@@ -23,4 +23,30 @@ const api = axios.create({
   },
 });
 
+// Example using Axios interceptor
+axios.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+    if (error.response.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+      try {
+        const response = await axios.get(
+          "http://localhost:4040/api/users/refreshToken"
+        );
+        if (response.status === 200) {
+          axios.defaults.headers.common["Authorization"] =
+            "Bearer " + response.data.accessToken;
+          originalRequest.headers["Authorization"] =
+            "Bearer " + response.data.accessToken;
+          return axios(originalRequest);
+        }
+      } catch (refreshError) {
+        console.error("Refresh token failed:", refreshError);
+      }
+    }
+    return Promise.reject(error);
+  }
+);
+
 export default api;

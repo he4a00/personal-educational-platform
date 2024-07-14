9c69bb166fce657f7328548cb050f329b8972a7d
he4a00 <https://github.com/he4a00>
2024-07-14 12:35:46 +0300

    refresh in the frontend

--- app/utils/api.ts
+++ app/utils/api.ts
@@ -1,15 +1,5 @@
 import axios from "axios";
 
-let parsedToken: string | null = null;
-
-if (typeof window !== "undefined") {
-  const userString = localStorage.getItem("user");
-  if (userString) {
-    const user = JSON.parse(userString);
-    parsedToken = user?.accessToken ?? null;
-  }
-}
-
 const API_BASE_URL =
   process.env.NODE_ENV === "development"
     ? process.env.NEXT_PUBLIC_API_BASE_URL_LOCAL
@@ -18,9 +8,55 @@ const API_BASE_URL =
 const api = axios.create({
   baseURL: API_BASE_URL,
   withCredentials: true,
-  headers: {
-    Authorization: parsedToken ? `Bearer ${parsedToken}` : undefined,
-  },
 });
+
+api.interceptors.request.use((config) => {
+  const userString = localStorage.getItem("user");
+  if (userString) {
+    const user = JSON.parse(userString);
+    const token = user?.accessToken;
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+  }
+  return config;
+});
+
+api.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+
+    if (error.response.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+      try {
+        const response = await axios.post(
+          `${API_BASE_URL}/refresh-token`,
+          {},
+          {
+            withCredentials: true,
+          }
+        );
+
+        const { accessToken, refreshToken } = response.data;
+
+        localStorage.setItem(
+          "user",
+          JSON.stringify({ accessToken, refreshToken })
+        );
+
+        api.defaults.headers.common.Authorization = `Bearer ${accessToken}`;
+        originalRequest.headers.Authorization = `Bearer ${accessToken}`;
+
+        return api(originalRequest);
+      } catch (refreshError) {
+        // Handle token refresh error, e.g., log out user, redirect to login
+        console.error("Token refresh failed", refreshError);
+        return Promise.reject(refreshError);
+      }
+    }
+    return Promise.reject(error);
+  }
+);
 
 export default api;

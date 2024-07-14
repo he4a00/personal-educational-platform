a17ac2cc939254c7c2e062508409aa8f9f95a18f
he4a00 <https://github.com/he4a00>
2024-07-14 12:43:58 +0300

    dsa

--- app/utils/api.ts
+++ app/utils/api.ts
@@ -50,7 +50,6 @@ api.interceptors.response.use(
 
         return api(originalRequest);
       } catch (refreshError) {
-        // Handle token refresh error, e.g., log out user, redirect to login
         console.error("Token refresh failed", refreshError);
         return Promise.reject(refreshError);
       }

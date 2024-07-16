a4dec860595f64429d5b176501b02109710fbbec
he4a00 <https://github.com/he4a00>
2024-07-16 18:00:51 +0300

    ds

--- app/components/HowItWork.tsx
+++ app/components/HowItWork.tsx
@@ -3,12 +3,12 @@ import Link from "next/link";
 
 const HowItWork = () => {
   return (
-    <div className="relative flex flex-col items-center w-full p-10 bg-white shadow-md rounded-lg mt-10">
+    <div className="relative flex flex-col items-center justify-center w-full p-10 bg-white shadow-md rounded-lg mt-10">
       <span className="absolute right-0 top-0 m-4 px-3 py-1 bg-red-600 text-white text-sm font-bold rounded-full">
         مهم جدا!
       </span>
       <div className="flex flex-col items-center container">
-        <h1 className="text-4xl font-bold p-5">كيف تعمل المنصة؟</h1>
+        <h1 className="text-4xl font-bold p-5 text-center">كيف تعمل المنصة؟</h1>
         <p className="text-xl font-semibold p-5 text-center">
           تعرف على كيفية عمل منصتنا من خلال هذا المقال المهم، حيث يوضح لك المسار
           الطبيعي للاستخدام وطريقة الدفع الخاصة بالدروس المدفوعة.

f5b24a8c904ad775dc43c03844026773f08029b3
he4a00 <https://github.com/he4a00>
2024-07-20 18:32:41 +0300

    fds

--- app/(root)/lessons/watch/[id]/page.tsx
+++ app/(root)/lessons/watch/[id]/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import Likes from "@/app/components/Likes";
+// import Likes from "@/app/components/Likes";
 import Loader from "@/app/components/Loader";
 import ToggleLike from "@/app/components/ToggleLike";
 import { useUserContext } from "@/app/context/UserContext";
@@ -74,10 +74,10 @@ const UnlockedLesson = () => {
             }}
           />
         </div>
-        <div className="p-5 mt-5 flex gap-5">
+        {/* <div className="p-5 mt-5 flex gap-5">
           <ToggleLike lessonId={id} />
           <Likes lessonId={id} />
-        </div>
+        </div> */}
       </div>
     </div>
   );

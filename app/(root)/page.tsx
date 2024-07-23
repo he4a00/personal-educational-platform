d88cb1d4544882482a48205e60c0abac043ed50d
he4a00 <https://github.com/he4a00>
2024-07-23 20:49:19 +0300

    fixed: the overflow of the announcement bar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -6,13 +6,14 @@ import Services from "../components/Services";
 export default function Home() {
   return (
     <main>
-      <div className="w-full flex items-center justify-center mt-3 bg-black text-yellow-300 h-14">
-        <div className="absolute h-2 animate-moving-text bg-black flex items-center w-full">
+      <div className="relative w-full overflow-hidden mt-3 bg-black text-yellow-300 h-14">
+        <div className="absolute top-0 left-0 h-full animate-moving-text flex items-center whitespace-nowrap">
           <h1 className="text-center">
             تجريبي : تم اضافة نظام اللايك علي جميع الدروس
           </h1>
         </div>
       </div>
+
       <Hero />
       <HowItWork />
       <Services />

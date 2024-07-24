8b5c63a7c0e9ec3e4417b71d504b08245485c42d
he4a00 <https://github.com/he4a00>
2024-07-25 02:20:29 +0300

    exam system announcement

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -8,9 +8,7 @@ export default function Home() {
     <main>
       <div className="relative w-full overflow-hidden mt-3 bg-black text-yellow-300 h-14">
         <div className="absolute top-0 left-0 h-full animate-moving-text flex items-center whitespace-nowrap">
-          <h1 className="text-center">
-            تجريبي : تم اضافة نظام اللايك علي جميع الدروس
-          </h1>
+          <h1 className="text-center">تم أضافة نظام الامتحان بالكامل.</h1>
         </div>
       </div>
 

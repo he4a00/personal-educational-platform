8a2ee5a4572b6fc03e8d8a601ac2b698b29cb5f8
he4a00 <https://github.com/he4a00>
2024-09-09 18:25:49 +0300

    new: style for services

--- app/components/Home/Services.tsx
+++ app/components/Home/Services.tsx
@@ -3,9 +3,9 @@ import { BookOpen, BrainCircuit, Rocket } from "lucide-react";
 
 export default function Services() {
   return (
-    <div className="w-full mt-44 mx-auto p-6 bg-gradient-to-br from-indigo-400 via-purple-400 to-pink-400 shadow-lg overflow-hidden relative">
+    <div className="w-full mt-44 mx-auto p-6 overflow-hidden relative">
       <div className="absolute inset-0 opacity-10 bg-[url('data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSI2MCIgaGVpZ2h0PSI2MCI+CjxyZWN0IHdpZHRoPSI2MCIgaGVpZ2h0PSI2MCIgZmlsbD0iI2ZmZiI+PC9yZWN0Pgo8cmVjdCB3aWR0aD0iNDIuNDIiIGhlaWdodD0iNDIuNDIiIHRyYW5zZm9ybT0idHJhbnNsYXRlKDMwLDMwKSByb3RhdGUoNDUpIiBmaWxsPSIjMDAwIiBmaWxsLW9wYWNpdHk9IjAuMDUiPjwvcmVjdD4KPC9zdmc+')]"></div>
-      <h1 className="text-4xl md:text-5xl font-bold text-center mb-8 text-white drop-shadow-lg">
+      <h1 className="text-4xl md:text-5xl font-bold text-center mb-8  drop-shadow-lg">
         🚀 ارتقِ بتعلمك! 🧠
       </h1>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
@@ -31,9 +31,6 @@ export default function Services() {
           textColor="text-green-900"
         />
       </div>
-      <Button className="mt-8 bg-white text-purple-600 font-bold py-3 px-6 rounded-full shadow-lg hover:shadow-xl transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105 mx-auto block">
-        ابدأ رحلتك الان!
-      </Button>
     </div>
   );
 }

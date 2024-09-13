84db045d61963f764c26a3e138af08794cc8ee44
he4a00 <https://github.com/he4a00>
2024-09-13 19:38:11 +0300

    ui changes for her section

--- app/components/Home/Hero.tsx
+++ app/components/Home/Hero.tsx
@@ -1,58 +1,58 @@
 "use client";
 
+import { motion } from "framer-motion";
 import { Button } from "@/components/ui/button";
-import { Book, UserRoundCheck } from "lucide-react";
 import Image from "next/image";
-import Link from "next/link";
-import hero from "../images/hero.png";
 import { useUserContext } from "@/app/context/UserContext";
+import Link from "next/link";
 
-const Hero = () => {
+export default function Hero() {
   const { user }: any = useUserContext();
   return (
-    <div className="flex flex-col md:flex-row justify-between items-center p-28 container">
-      <div>
-        <Image
-          width={600}
-          height={600}
-          alt="hero image"
-          src="https://res.cloudinary.com/dortdlynv/image/upload/v1720606966/hero_k7droq.png"
-        />
-      </div>
-      <div className="flex flex-col gap-2">
-        <h1 className="text-5xl font-bold  text-gray-500">
-          الاستاذ في الرياضيات
-        </h1>
-        <h6 className="md:text-5xl text-3xl font-semibold text-gray-500 p-3 ">
-          هاشم شكري
-        </h6>
-        <p className="md:text-3xl text-xl font-semibold text-gray-500 p-3 ">
-          الرياضيات بعيون جديدة استكشاف بلا حدود.
-        </p>
-        {!user ? (
-          <Link href="/sign-in">
-            <Button
-              variant="destructive"
-              className="flex gap-2 p-7 transition-all duration-300 ease-in-out transform hover:shadow-md hover:translate-y-0.5"
-            >
-              انضم الان
-              <UserRoundCheck />
-            </Button>
-          </Link>
-        ) : (
-          <Link href={`/lessons/user/${user?.user?._id}`}>
-            <Button
-              variant="destructive"
-              className="flex gap-2 p-7 transition-all duration-300 ease-in-out transform hover:shadow-md hover:translate-y-0.5"
-            >
-              دروسي
-              <Book />
-            </Button>
-          </Link>
-        )}
+    <div className="relative min-h-screen overflow-hidden container pt-28">
+      <div className="container mx-auto px-4 py-16 flex flex-col lg:flex-row items-center justify-between relative z-10 gap-y-5">
+        {/* Left section with text */}
+        <div className="hidden md:flex">
+          <Image
+            width={600}
+            height={600}
+            alt="hero image"
+            src="https://res.cloudinary.com/dortdlynv/image/upload/v1720606966/hero_k7droq.png"
+          />
+        </div>
+        <div className="lg:w-1/2 mb-10 lg:mb-0">
+          <h1 className="text-5xl lg:text-7xl font-bold mb-4 text-gray-800 leading-tight">
+            <span className="inline-block transform -rotate-3 text-blue-600">
+              الأستاذ
+            </span>{" "}
+            <span className="inline-block transform rotate-2 text-green-600">
+              في الرياضيات،
+            </span>
+            <br />
+            <span className="inline-block transform -rotate-1 text-red-600">
+              هاشم شكري
+            </span>
+          </h1>
+          <p className="text-xl lg:text-2xl mb-8 text-gray-700 font-light mt-2">
+            <span className="inline-block transform -rotate-3 text-green-600 font-semibold">
+              استكشف الرياضيات
+            </span>
+            <br />
+            <span className="inline-block transform -rotate-3 text-red-600 font-semibold">
+              بعيون جديدة{" "}
+            </span>
+          </p>
+          <motion.div whileHover={{ scale: 1.05 }} whileTap={{ scale: 0.95 }}>
+            {!user && (
+              <Link href="/sign-in">
+                <Button className="bg-gradient-to-r from-red-500 to-pink-500 hover:from-red-600 hover:to-pink-600 text-white px-10 py-4 rounded-full text-xl shadow-lg transition duration-300 ease-in-out transform hover:-translate-y-1">
+                  انضم الان
+                </Button>
+              </Link>
+            )}
+          </motion.div>
+        </div>
       </div>
     </div>
   );
-};
-
-export default Hero;
+}
